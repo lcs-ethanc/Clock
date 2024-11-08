214c5bb496a15901ac4ef11cc0078b0768ed2b36
Ethan Chen <echen@lcs.on.ca>
2024-11-08 09:10:07 -0500

    got tab view set up

---
+++ Clock/Views/StopWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopWatchView.swift
+//  Clock
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-08.
+//
+
+import SwiftUI
+
+struct StopWatchView: View {
+    var body: some View {
+        Text("Stopwatch")
+    }
+}
+
+#Preview {
+    StopWatchView()
+}
---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-08.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text("World Clock")
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
