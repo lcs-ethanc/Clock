35899498bd72132078f8e2446565dab509c88704
Ethan Chen <echen@lcs.on.ca>
2024-11-08 09:59:00 -0500

    finished h stacks

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -14,7 +14,7 @@ struct WorldClockView: View {
                 HStack{
                     
                     VStack(alignment: .leading){
-                        Text("Today, 0+ hours")
+                        Text("Today, +0HRS")
                             .font(.system(size: 15))
                             .foregroundColor(.gray)
                         Text("Ottawa")
@@ -32,6 +32,118 @@ struct WorldClockView: View {
                     }
                     
                 }
+                .padding(.bottom,0.5)
+                HStack{
+                    
+                    VStack(alignment: .leading){
+                        Text("Today, -3HRS")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                        Text("Vancouver")
+                            .font(.system(size: 30))
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .firstTextBaseline){
+                        Text("3:35")
+                            .fontWeight(.thin)
+                            .font(.system(size: 60))
+                        Text("AM")
+                            .fontWeight(.thin)
+                            .font(.system(size: 40))
+                    }
+                    
+                }
+                .padding(.bottom,0.5)
+                HStack{
+                    
+                    VStack(alignment: .leading){
+                        Text("Today, +6HRS")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                        Text("Hamburg")
+                            .font(.system(size: 30))
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .firstTextBaseline){
+                        Text("12:35")
+                            .fontWeight(.thin)
+                            .font(.system(size: 60))
+                        Text("PM")
+                            .fontWeight(.thin)
+                            .font(.system(size: 40))
+                    }
+                    
+                }
+                .padding(.bottom,0.5)
+                HStack{
+                    
+                    VStack(alignment: .leading){
+                        Text("Today, +16HRS")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                        Text("Sydney")
+                            .font(.system(size: 30))
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .firstTextBaseline){
+                        Text("10:35")
+                            .fontWeight(.thin)
+                            .font(.system(size: 60))
+                        Text("PM")
+                            .fontWeight(.thin)
+                            .font(.system(size: 40))
+                    }
+                    
+                }
+                .padding(.bottom,0.5)
+                HStack{
+                    
+                    VStack(alignment: .leading){
+                        Text("Today, +9HRS")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                        Text("Dubai")
+                            .font(.system(size: 30))
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .firstTextBaseline){
+                        Text("3:35")
+                            .fontWeight(.thin)
+                            .font(.system(size: 60))
+                        Text("PM")
+                            .fontWeight(.thin)
+                            .font(.system(size: 40))
+                    }
+                    
+                }
+                .padding(.bottom,0.5)
+                HStack{
+                    
+                    VStack(alignment: .leading){
+                        Text("Today, -1HRS")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                        Text("Mexico City")
+                            .font(.system(size: 30))
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .firstTextBaseline){
+                        Text("5:35")
+                            .fontWeight(.thin)
+                            .font(.system(size: 60))
+                        Text("AM")
+                            .fontWeight(.thin)
+                            .font(.system(size: 40))
+                    }
+                    
+                }
+                .padding(.bottom,0.5)
+                Spacer()
             }
             
             .navigationTitle("World Clock")
